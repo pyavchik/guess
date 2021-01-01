2746fb3ac9aba2f84fd0278bd6a2fd805e6dde0d
pyavchik <pyavchik@gmail.com>
2021-01-01 13:44:46 +0200

    language changed to en

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.print("Guess number: ");
         Scanner scanner = new Scanner(System.in);
         while (!scanner.hasNextInt()){
-            System.out.println("only digits possible");
+            System.out.println("Only digits possible");
             System.out.print("Guess number: ");
             scanner = new Scanner(System.in);
         }
@@ -22,26 +22,26 @@ public class Main {
                 System.out.print("Guess number: ");
                 scanner = new Scanner(System.in);
                 while (!scanner.hasNextInt()){
-                    System.out.println("only digits possible");
+                    System.out.println("Only digits possible");
                     System.out.print("Guess number: ");
                     scanner = new Scanner(System.in);
                 }
                 answer = scanner.nextInt();
             }
             if (answer < numberToGuess) {
-                System.out.printf("Secret number is more, than %d спробуйте в діапазоні [%d, %d] \n", answer, answer, max);
+                System.out.printf("Secret number is more, than %d try a number [%d, %d] \n", answer, answer, max);
                 min = answer;
                 System.out.print("Guess number: ");
                 scanner = new Scanner(System.in);
                 while (!scanner.hasNextInt()){
-                    System.out.println("only digits possible");
+                    System.out.println("Only digits possible");
                     System.out.print("Guess number: ");
                     scanner = new Scanner(System.in);
                 }
                 answer = scanner.nextInt();
             }
             if (answer > numberToGuess) {
-                System.out.printf("Secret number less, than %d спробуйте в діапазоні [%d, %d] \n", answer, min, answer);
+                System.out.printf("Secret number less, than %d try a number [%d, %d] \n", answer, min, answer);
                 max = answer;
                 System.out.print("Guess number: ");
                 scanner = new Scanner(System.in);
